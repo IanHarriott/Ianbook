f877ed0e088704d8cf1fd975cd6916967839e845
IanHarriott <warriorbydawn@hotmail.com>
2020-01-26 21:08:09 -0800

    Added production static code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //db
-const connectionString = process.env.MONGO_URI || "";
+const connectionString = process.env.MONGO_URI || '';
 mongoose
 	.connect(connectionString, {
 		useUnifiedTopology: true,
@@ -56,6 +56,19 @@ app.use(function(err, req, res, next) {
 		res.status(401).json({ error: 'Unauthorized!' });
 	}
 });
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static fold
+	console.log(path.resolve(__dirname, '../client/build'));
+	app.use(__dirname, express.static('../client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, '../client', 'build', 'index.html')
+		);
+	});
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
